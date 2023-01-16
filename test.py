e31d4a64244928ce05ba54dd3ff8ddbbefd49645
aitymori <anakholodko@gmail.com>
2023-01-17 00:30:04 +0200

    Виводить всі контакти. Але не генератором.

--- test.py
+++ test.py
@@ -3,21 +3,24 @@ from datetime import datetime
 
 
 class Field():
+    pass
+    # def __init__(self, value=None):
+    #     self.__private_value = None
+    #     self.value = value
+
+    # @property
+    # def value(self):
+    #     return self.__private_value
     
-    def __init__(self, value=None):
-        self.__private_value = None
-        self.value = value
-
-    @property
-    def value(self):
-        return self.__private_value
-
 
 class Name(Field):
 
     def __init__(self, name: str):
         self.__private_name = None
         self.name = name
+
+    def __repr__(self):
+        return f'Name: {self.name}'
 
     @property
     def name(self):
@@ -33,26 +36,29 @@ class Name(Field):
 
 class Phone(Field):
     
-    # def __init__(self, phone):
-    #     self.__private_value = None
-    #     self.value = phone
+    def __init__(self, phone=None):
+        self.__private_value = None
+        self.value = phone
 
-    # @property
-    # def value(self):
-    #     return self.__private_value
+    def __repr__(self):
+        return f'{self.value}'
 
+    @property
+    def value(self):
+        return self.__private_value
 
     # перевірка коректності номера. Додавання +38 
-    @Field.value.setter    
-    # @value.setter
+    # @Field.value.setter    
+    @value.setter
     def value(self, value):
-        if value.isdigit():
+        if value.isdigit() == True:
             if len(value) == 12:
                 self.__private_value = "+" + value
-        elif len(value) < 12:
+            elif len(value) < 12:
                 self.__private_value = "+38" + value
         else:
             raise Exception ('Wrong number.')
+        
         
 
 class Birthday(Field):
@@ -69,16 +75,19 @@ class Birthday(Field):
     @value.setter
     def value(self, value: str):
         try:
-            self.__private_value = datetime.strptime(value, '%d/%m/%Y')
+            self.__private_value = datetime.strptime(value, '%d/%m/%Y').date()
         except ValueError:
             print (f'Please, input the date in format dd/mm/yyyy ')     
 
 
 class Record():
 
-    def __init__(self, name: Name, *phones):
+    def __init__(self, name: Name, birthday: Birthday, *phones):
         self.name = name
         self.phones = list(phones)
+        self.birthday = birthday
+    def __repr__(self):
+        return f'{self.name}, phones: {self.phones}, birthday: {self.birthday.value}'
 
     def add_phone(self, value: Phone):
         self.phones.append(value)
@@ -95,51 +104,67 @@ class Record():
         self.birthday = birthday
         if self.birthday.value:
             try:
-                current_date = datetime.now()
-                self.birthday = self.birthday.value.replace(year = current_date.year)
-                delta_days = self.birthday - current_date
+                current_date = datetime.now().date()
+                user_date = self.birthday.value.replace(year = current_date.year)
+                delta_days = user_date - current_date
                     
                 if 0 < delta_days.days:
                     return f'Лишилось до Дня народження: {delta_days.days} днів.'
                 else:
-                    self.birthday = self.birthday.value.replace(year=self.birthday.value.year + 1)
-                    delta_days = self.birthday - current_date
+                    user_date = self.birthday.value.replace(year=user_date.year + 1)
+                    delta_days = user_date - current_date
                     if 0 < delta_days.days:
                         return f'Лишилось до Дня народження: {delta_days.days} днів.'
             except ValueError:
                 return f'Please, input date in format dd/mm/yyyy '
         else:
             return f'Date of birth is not found. Please, add day of birth, if you want. '
-        
+
 
 class AddressBook(UserDict):
 
+    def __repr__(self):
+        
+        counter = 0
+        for key, value in self.data.items():
+            print(f'{value}')
+            counter +=1
+        return (f'Printed {counter} contacts.')
+
     def add_record(self, record: Record):
         self.data[record.name.name] = record
-
+    
 
 if __name__ == '__main__':
+    ab = AddressBook()
     name = Name('Bill')
     phone = Phone('1234567890')
-    birth = Birthday('26/03/2002')
-    rec = Record(name, phone, birth)
-    ab = AddressBook()
+    birth = Birthday('26/02/2002')
+    rec = Record(name, birth, phone)
     ab.add_record(rec)
-    
+
     assert isinstance(ab['Bill'], Record)
     assert isinstance(ab['Bill'].name, Name)
     assert isinstance(ab['Bill'].phones, list)
     assert isinstance(ab['Bill'].phones[0], Phone)
     print(ab['Bill'].days_to_birthday(birth))
-    assert ab['Bill'].phones[0].value == '1234567890'
+    assert ab['Bill'].phones[0].value == '+381234567890'
     
     print('All Ok)')
 
+    name = Name('Nasik')
+    phone = Phone('0671603025')
+    birth = Birthday('26/03/2002')
+    rec = Record(name, birth, phone)
+    ab.add_record(rec)
+    name = Name('Nasiya')
+    phone = Phone('0554603025')
+    birth = Birthday('22/01/2010')
+    rec = Record(name, birth, phone)
+    ab.add_record(rec)
 
+    print(ab)
 
-# setter та getter логіку для атрибутів value спадкоємців Field.
-# Перевірку на коректність веденого номера телефону setter для value класу Phone.
-# Перевірку на коректність веденого дня народження setter для value класу Birthday.
 
 # AddressBook реалізує метод iterator, який повертає генератор за записами AddressBook
 # і за одну ітерацію повертає уявлення для N записів.
